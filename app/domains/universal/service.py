b45f93f5d166414e6bf188bf1741ed73268946ff
Arsen Sarkisyan <s-arsen01@mail.ru>
2025-08-20 00:10:57 +0300

    fix: добавил фильтрацию по наночастице в новые методы

--- app/domains/universal/service.py
+++ app/domains/universal/service.py
@@ -7,19 +7,21 @@ from app.domains.common.service_map import SERVICE_MAP
 
 class UniversalDataService:
     @staticmethod
-    def get_data(db: Session, domain: str, data_type: str):
-        # Находим нужный класс сервиса
+    def get_data(
+        db: Session, domain: str, data_type: str, nanoparticle: str | None = None
+    ):
         service_class = SERVICE_MAP.get(domain)
         if not service_class:
             raise ValueError(f"Домен '{domain}' не найден.")
 
-        # Находим нужное имя метода
         method_name = METHOD_MAP.get(data_type)
         if not method_name:
             raise ValueError(f"Тип данных '{data_type}' не найден.")
 
-        # Получаем сам метод из класса
         method_to_call = getattr(service_class, method_name)
-
-        # Вызываем метод
-        return method_to_call(db)
+        # Мы передаем nanoparticle только если тип данных all_data,
+        if data_type == "all_data":
+            return method_to_call(db=db, nanoparticle=nanoparticle)
+        else:
+            # Для всех остальных типов просто вызываем метод без фильтра
+            return method_to_call(db=db)
